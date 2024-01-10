67c27e8605593a38dddd43099dbe4f41482da877
mahjs <mahjsdev@gamil.com>
2024-01-10 11:33:21 +0330

    make add service work accordingly

--- src/components/dashboard/AddDomainModal.tsx
+++ src/components/dashboard/AddDomainModal.tsx
@@ -1,10 +1,9 @@
 import { Modal, Box, Stack, Typography, Button } from "@mui/material";
-import { GoPlus } from "react-icons/go";
 import { RxCross2 } from "react-icons/rx";
-import convertToPersianWordNumber from "../../utils/convertToPersianWordNumber";
 import { FC, useState } from "react";
 import api from "../../services";
 import Input from "../login/Input";
+import axios from "axios";
 
 interface Props {
   openModal: boolean;
@@ -17,31 +16,38 @@ const AddDomainModal: FC<Props> = ({
   openModal,
   setOpenModal,
   refetchDomains,
-  refetchIpAddresses,
+  refetchIpAddresses
 }) => {
-  const [ipAddressCountForAdding, setIpAddressCountForAdding] =
-    useState<number>(1);
-
   const [domainName, setDomainName] = useState<string>("");
-  const [ipAddresses, setIpAddresses] = useState<string[]>([]);
 
   const handleSubmit = () => {
     if (!domainName || domainName.length === 0) return;
     api.domain.addDomain([domainName]).then(() => {
       refetchDomains();
-      if (ipAddresses.length === 0) {
-        setOpenModal(false);
-        setDomainName("");
-        return;
-      }
-      api.domain.addIpAddressesToDomain(domainName, ipAddresses).then(() => {
-        refetchIpAddresses();
-        setOpenModal(false);
-        setDomainName("");
-        setIpAddresses([]);
-        setIpAddressCountForAdding(1);
-      });
+      setOpenModal(false);
+      setDomainName("");
+      return;
     });
+
+    const addedDomainName = domainName + ".com";
+    axios
+      .post("http://10.201.228.64:5000/get_ipv4", {
+        domains: [
+          {
+            name: domainName + ".com"
+          }
+        ]
+      })
+      .then((res) => {
+        api.domain
+          .addIpAddressesToDomain(domainName, res.data[addedDomainName])
+          .then(() => {
+            refetchIpAddresses();
+            refetchDomains();
+            setOpenModal(false);
+            setDomainName("");
+          });
+      });
   };
   return (
     <Modal
@@ -55,7 +61,7 @@ const AddDomainModal: FC<Props> = ({
         zIndex: "1000",
         display: "flex",
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
       }}
     >
       <Box
@@ -65,7 +71,7 @@ const AddDomainModal: FC<Props> = ({
           borderRadius: "1.5rem",
           width: "300px",
           paddingY: "1rem",
-          paddingX: "2rem",
+          paddingX: "2rem"
         }}
       >
         <Box
@@ -73,7 +79,7 @@ const AddDomainModal: FC<Props> = ({
             display: "flex",
             flexDirection: "column",
             gap: ".5rem",
-            marginBottom: "1rem",
+            marginBottom: "1rem"
           }}
         >
           <Stack
@@ -84,18 +90,17 @@ const AddDomainModal: FC<Props> = ({
             <Typography
               sx={{
                 fontSize: "1.4rem",
-                fontWeight: "900",
+                fontWeight: "900"
               }}
             >
               افزودن سرویس
             </Typography>
             <Button
               onClick={() => {
-                setIpAddressCountForAdding(1);
                 setOpenModal(false);
               }}
               sx={{
-                color: "#000",
+                color: "#000"
               }}
             >
               بستن پنجره
@@ -113,44 +118,15 @@ const AddDomainModal: FC<Props> = ({
           sx={{
             display: "flex",
             flexDirection: "column",
-            gap: "1rem",
+            gap: "1rem"
           }}
         >
           <Input
             value={domainName}
             onChange={(value) => setDomainName(value)}
             name="domain_name"
-            title="آدرس وبسایت"
+            title="نام وبسایت"
           />
-          {Array(ipAddressCountForAdding)
-            .fill(null)
-            .map((_, index) => (
-              <Input
-                value={ipAddresses[index]}
-                onChange={(value) =>
-                  setIpAddresses((prev) => {
-                    const newIpAddresses = [...prev];
-                    newIpAddresses[index] = value;
-                    return newIpAddresses;
-                  })
-                }
-                key={index}
-                name="domain_name"
-                title={
-                  "آدرس IP " +
-                  convertToPersianWordNumber(ipAddressCountForAdding)
-                }
-              />
-            ))}
-          <Button
-            sx={{
-              marginLeft: "auto",
-            }}
-            onClick={() => setIpAddressCountForAdding((prev) => (prev += 1))}
-          >
-            <GoPlus />
-            افزودن آدرس IP بیشتر
-          </Button>
           <Button
             type="submit"
             sx={{
@@ -159,8 +135,8 @@ const AddDomainModal: FC<Props> = ({
               borderRadius: ".5rem",
               ":hover": {
                 background: "#0F6CBD",
-                color: "#fff",
-              },
+                color: "#fff"
+              }
             }}
           >
             افزودن
