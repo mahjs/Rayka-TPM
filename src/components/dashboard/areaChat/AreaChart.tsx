822ca344c53253c0a30ddd16a4553d15384729a5
mahjs <mahjsdev@gmail.com>
2024-01-17 10:04:46 +0330

    Area chart fix

--- src/components/dashboard/areaChat/AreaChart.tsx
+++ src/components/dashboard/areaChat/AreaChart.tsx
@@ -362,8 +362,8 @@ const AreaChart: FC<Props> = ({ isAllDataLoaded }) => {
             transition: "opacity .3s ease",
             gap: "1rem",
             position: "absolute",
-            top: "1rem",
-            left: "52%",
+            top: ".3rem",
+            right: "-6.2rem",
             transform: "translateX(-50%)",
             background: "#fff",
             zIndex: "40",
@@ -396,7 +396,16 @@ const AreaChart: FC<Props> = ({ isAllDataLoaded }) => {
                 return tick; // For other ticks, display their actual value
               }}
             />
-            <XAxis padding={{ left: 40, right: 60 }} dataKey="time" />
+            <XAxis
+              padding={{ left: 40, right: 60 }}
+              dataKey={
+                selectedTimeForAreaChart === "Week" ||
+                selectedTimeForAreaChart === "Year" ||
+                selectedTimeForAreaChart === "Month"
+                  ? "date"
+                  : "time"
+              }
+            />
             <defs>
               <filter id="glow" x="-70%" y="-70%" width="200%" height="200%">
                 <feOffset result="offOut" in="SourceGraphic" dx="0" dy="0" />
--- src/utils/formatDateForAreaChart.ts
+++ src/utils/formatDateForAreaChart.ts
@@ -5,5 +5,9 @@ export default function formatDateForAreaChart(date: Date) {
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
 
-  return [`${hours}:${minutes}`, `${year}-${month}-${day} ${hours}:${minutes}`];
+  return [
+    `${hours}:${minutes}`,
+    `${year}-${month}-${day}`,
+    `${year}-${month}-${day} ${hours}:${minutes}`
+  ];
 }
